b8a2292e09a3a07b8fde4b29aef3aca1d315e657
Laurent Gatto <laurent.gatto@uclouvain.be>
2025-03-24 12:40:21 +0100

    add more code

---
+++ bioc-intro/S01-startr.R
@@ -0,0 +1,122 @@
+1 + 11
+
+## Variables
+
+x <- 2 * 123
+
+x
+
+y <- 2
+
+weight_kg <- 67
+
+
+2 * weight_kg
+
+weight_kg <- 2 * weight_kg
+
+2 * x
+
+2 * k
+
+k
+
+## Exercice
+
+mass <- 47.5            # mass?
+age <- 122             # age?
+mass <- mass * 2.0      # mass?
+age  <- age - 20        # age?
+mass_index <- mass / age  # mass_index?
+
+age < - 122
+
+## Functions
+
+ls() 
+
+round(3.14)
+
+?round
+
+round(3.14, digits = 1)
+
+k <- round(3.14, 1)
+k
+
+## Vectors
+
+
+weight_g <- c(60, 50, 65, 82)
+
+length(weight_g)
+class(weight_g)
+
+
+molecules <- c("dna", "rna", "protein")
+molecules
+length(molecules)
+class(molecules)
+
+c(20, weight_g)
+
+lw <- 
+  length(  
+    c( 20, weight_g, 100 )  
+    )
+
+class(c(20, weight_g, 100))
+
+
+10
+
+molecules
+
+class("1")
+
+c(1, 2)
+
+c("1", "2")
+
+c("1", 2)
+
+as.numeric("1")
+
+as.numeric("a")
+
+TRUE
+FALSE
+
+length(c(TRUE, FALSE))
+class(c(TRUE, FALSE))
+
+## Ex
+
+num_char <- c(1, 2, 3, "a")
+num_logical <- c(1, 2, 3, TRUE, FALSE)
+char_logical <- c("a", "b", "c", TRUE)
+tricky <- c(1, 2, 3, "4")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
