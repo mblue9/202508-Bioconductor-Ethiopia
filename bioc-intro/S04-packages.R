81c3e50558940558b7ca056de5d2d55d4208fa98
Laurent Gatto <laurent.gatto@uclouvain.be>
2025-03-26 08:48:21 +0300

    add install script

---
+++ bioc-intro/S04-packages.R
@@ -0,0 +1,13 @@
+## CRAN package installation
+## See https://carpentries-incubator.github.io/bioc-intro/20-r-rstudio.html#r-packages
+
+
+## CRAN = Comprehensive R Archive Network
+
+
+
+## Bioconductor package installation
+
+
+
+
