068109239834c2234fd5f50afca420e39e68595b
Laurent Gatto <laurent.gatto@uclouvain.be>
2025-03-26 08:56:15 +0300

    more install

--- bioc-intro/S04-packages.R
+++ bioc-intro/S04-packages.R
@@ -11,3 +11,14 @@
 
 
 
+## All packages
+## See https://carpentries-incubator.github.io/bioc-rnaseq/
+BiocManager::install(c("tidyverse", "SummarizedExperiment",
+                       "ExploreModelMatrix", "AnnotationDbi", "org.Hs.eg.db", 
+                       "org.Mm.eg.db", "csoneson/ConfoundingExplorer",
+                       "DESeq2", "vsn", "ComplexHeatmap", "hgu95av2.db",
+                       "RColorBrewer", "hexbin", "cowplot", "iSEE",
+                       "clusterProfiler", "enrichplot", "kableExtra",
+                       "msigdbr", "gplots", "ggplot2", "simplifyEnrichment",
+                       "apeglm", "microbenchmark", "Biostrings",
+                       "SingleCellExperiment"))
