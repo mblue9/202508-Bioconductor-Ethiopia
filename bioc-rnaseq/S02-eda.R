c48e49494d19182a17a889e2faefb4b6ad835760
Laurent Gatto <laurent.gatto@uclouvain.be>
2025-03-28 13:07:11 +0300

    more eda

--- bioc-rnaseq/S02-eda.R
+++ bioc-rnaseq/S02-eda.R
@@ -1,6 +1,10 @@
 ## S02-eda.R
 
 library(SummarizedExperiment)
+library(tidyverse)
+library(DESeq2)
+library(vsn)
+
 rse <- readRDS("data/rse.rds")
 
 ## Ex: Find and remove unexpressed genes.
@@ -40,8 +44,84 @@ table(rs > 10)
 table(rs > 25)
 
 
-## Compute the library sizes, i.e the sum of counts for each sample.
+## Compute the library sizes, i.e the sum
+## of counts for each sample.
 
-## Add the calculation to the colData (call the new colData variable libSize).
+libSize <- colSums(assay(se))
 
-## Extract the colData and visualsie the library sizes.
+## Add the calculation to the colData
+## (call the new colData variable libSize).
+
+colData(se)$libSize <- colSums(assay(se))
+
+colData(se)$libSize
+se$libSize
+
+## Visualize the library sizes.
+
+View(libSize)
+
+colData(se) %>% 
+  as.data.frame() %>% 
+  ggplot(aes(
+   x = geo_accession, 
+   y = libSize,
+   fill = time
+   )) +
+  geom_bar(stat = "identity")
+
+
+colData(se) %>% 
+  as.data.frame() %>% 
+  ggplot(aes(
+    x = geo_accession, 
+    y = libSize,
+    fill = sex
+  )) +
+  geom_bar(stat = "identity")
+
+
+
+
+## Compute/estimate size factors
+library(DESeq2)
+
+names(colData(se))
+
+dds <- DESeqDataSet(se, design = ~ sex + time)
+
+dds <- estimateSizeFactors(dds)
+
+colData(dds)
+
+## Visualize the size factors.
+
+colData(dds) %>% 
+  as.data.frame() %>% 
+  ggplot(aes(
+    x = geo_accession, 
+    y = sizeFactor,
+    fill = sex
+  )) +
+  geom_bar(stat = "identity")
+
+colData(dds) %>% 
+  as.data.frame() %>% 
+  ggplot(aes(
+    x = geo_accession, 
+    y = sizeFactor,
+    fill = time
+  )) +
+  geom_bar(stat = "identity")
+
+
+## Transformation
+
+library(vsn)
+
+meanSdPlot
+
+
+
+
+
